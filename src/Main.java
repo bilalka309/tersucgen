8657a34f41060c605a459974955e0033af2cb936
bilal karahan <bilalkarahan309@gmail.com>
2022-09-24 22:16:23 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in=new Scanner(System.in);
+        System.out.println("Basamak sayısını giriniz : ");
+        int x= in.nextInt();
+        for(int i=x ;i>0;i--){
+            for(int k=x;i<k;k--){
+                System.out.print(" ");
+            }
+
+            for(int k=1;k<=(2*i)-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
